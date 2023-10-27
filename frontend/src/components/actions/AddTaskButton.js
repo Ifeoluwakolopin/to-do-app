6f4058ae140cb95a006ccdc0016fa22d9fa8ce7e
Ifeoluwakolopin <areifeoluwa10@gmail.com>
2023-10-26 23:07:43 -0300

    refactor buttons2

--- frontend/src/components/actions/AddTaskButton.js
+++ frontend/src/components/actions/AddTaskButton.js
@@ -3,17 +3,19 @@ import AddTask from '../AddTask';
 import { OverlayTrigger, Tooltip, Button, Modal } from 'react-bootstrap';
 import { FaPlus } from 'react-icons/fa'; 
 
-
-
 export default function AddTaskButton({ listId, itemId, onTaskAdded }) {
     const [showAddSubtaskModal, setShowAddSubtaskModal] = useState(false);
+
+    const isFromTask = !!itemId; // Check if itemId is provided
+    const tooltipText = isFromTask ? "Add Sub-Task" : "Add Task";
+    const modalTitle = isFromTask ? "Add Sub-Task" : "Add Task";
 
     const handleOpenAddSubtaskModal = () => setShowAddSubtaskModal(true);
     const handleCloseAddSubtaskModal = () => setShowAddSubtaskModal(false);
 
     return (
         <>
-            <OverlayTrigger overlay={<Tooltip id="tooltip-add">Add Sub-Task</Tooltip>}>
+            <OverlayTrigger overlay={<Tooltip id="tooltip-add">{tooltipText}</Tooltip>}>
                 <Button variant="link" className="btn-sm p-0 text-decoration-none mr-3" onClick={handleOpenAddSubtaskModal}>
                     <FaPlus size={10} color="#0056b3" />
                 </Button>
@@ -21,7 +23,7 @@ export default function AddTaskButton({ listId, itemId, onTaskAdded }) {
 
             <Modal show={showAddSubtaskModal} onHide={handleCloseAddSubtaskModal}>
                 <Modal.Header closeButton>
-                    <Modal.Title>Add Sub-Task</Modal.Title>
+                    <Modal.Title>{modalTitle}</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                     <AddTask
